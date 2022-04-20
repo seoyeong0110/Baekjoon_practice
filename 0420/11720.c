bd24038ceac455e9a7150e4d268bd6103dc6a4eb
seoyeong0110 <seoyeong0110@naver.com>
2022-04-20 17:27:58 +0900

    0420

---
+++ 0420/11720.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+	int N, sum = 0;
+	scanf("%d",&N);
+	char num[N];
+	scanf("%s",&num);
+	for(int i = 0; i < N ; i++){
+		sum+=(int)num[i]-48;
+	}
+	printf("%d",sum);
+	return 0;
+} 
